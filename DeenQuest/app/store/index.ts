91f02b4a03a64d546103191e44429d102130f463
M-awais-rasool <ar30781871@gmail.com>
2026-04-14 19:38:20 +0500

    refactor: remove ReflectionScreen and RewardsScreen components along with related Redux store files
    - Deleted ReflectionScreen.tsx and RewardsScreen.tsx to streamline the app's structure.
    - Removed hooks, index, services, and slices related to Redux store management.
    - Cleaned up the store configuration by eliminating unused code and dependencies.

--- DeenQuest/app/store/index.ts
+++
@@ -1,6 +0,0 @@
-export { store } from './store';
-export type { RootState, AppDispatch } from './store';
-export { useAppDispatch, useAppSelector } from './hooks';
-export * from './slices/mainSlice';
-export { API } from './services/api';
-export * from './services/api';
