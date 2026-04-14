7058f7665694e6dce766dfffc3f0a37b065c1ecf
M-awais-rasool <ar30781871@gmail.com>
2026-04-14 19:43:57 +0500

    Add initial project setup with main application files
    - Created App.js as the main entry point for the application, integrating Redux and SafeAreaProvider.
    - Added app.json for Expo configuration, including app metadata and splash screen settings.
    - Implemented index.js to register the root component with Expo.

---
+++ DeenQuestExpo/App.js
@@ -0,0 +1,24 @@
+import {
+  initialWindowMetrics,
+  SafeAreaProvider,
+} from "react-native-safe-area-context";
+import { Provider } from "react-redux";
+import { AppNavigator } from "./app/navigators/AppNavigator";
+import { store } from "./app/store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <SafeAreaProvider
+        initialMetrics={initialWindowMetrics}
+        style={{ flex: 1 }}
+      >
+        {/* <KeyboardProvider> */}
+        <AppNavigator />
+        {/* </KeyboardProvider> */}
+      </SafeAreaProvider>
+    </Provider>
+  );
+}
+
+export default App;
