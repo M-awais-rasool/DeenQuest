1179fb724dab73dd27abb9f4a54cdb7dd5508153
M-awais-rasool <ar30781871@gmail.com>
2026-05-11 14:08:14 +0500

    fix: update base URL for API requests

--- DeenQuestExpo/app/store/services/api.ts
+++ DeenQuestExpo/app/store/services/api.ts
@@ -4,7 +4,7 @@ import { STORAGE_KEYS } from "../storage/authStorage";
 
 // Base query with auth handling
 const baseQueryWithAuth = fetchBaseQuery({
-  baseUrl: "http://172.16.27.85:8080",
+  baseUrl: "http://172.16.29.205:8080",
   prepareHeaders: async (headers) => {
     try {
       const token = await AsyncStorage.getItem(STORAGE_KEYS.accessToken);
