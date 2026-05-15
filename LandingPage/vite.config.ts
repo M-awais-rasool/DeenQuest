940cca62c46ef11b6d40a8adf61af4ef6d3162d0
M-awais-rasool <ar30781871@gmail.com>
2026-05-15 17:52:44 +0500

    fix plaginn issue

--- LandingPage/vite.config.ts
+++ LandingPage/vite.config.ts
@@ -1,21 +1,19 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
-    tailwindcss(),
-    tsconfigPaths(),
-    react(),
     tanstackStart({
       server: {
         entry: "./src/server.ts",
       },
     }),
+    tailwindcss(),
+    tsconfigPaths(),
   ],
   server: {
     port: 3000,
