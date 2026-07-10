a2860490ff091824e5f0c5cd9861d88e5070d36c
M-awais-rasool <ar30781871@gmail.com>
2026-07-11 03:05:46 +0500

    Add Google Fonts dependencies for Amiri and Nunito

--- DeenQuestExpo/App.js
+++ DeenQuestExpo/App.js
@@ -1,4 +1,17 @@
 import {
+  Amiri_400Regular,
+  Amiri_700Bold,
+} from "@expo-google-fonts/amiri";
+import {
+  Nunito_400Regular,
+  Nunito_500Medium,
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+  Nunito_900Black,
+  useFonts,
+} from "@expo-google-fonts/nunito";
+import {
   initialWindowMetrics,
   SafeAreaProvider,
 } from "react-native-safe-area-context";
@@ -8,6 +21,21 @@ import { AppNavigator } from "./app/navigators/AppNavigator";
 import { store } from "./app/store";
 
 function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_400Regular,
+    Nunito_500Medium,
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+    Nunito_900Black,
+    Amiri_400Regular,
+    Amiri_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <Provider store={store}>
       <SafeAreaProvider
